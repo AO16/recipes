af585c134edaf02e70e4d56f4b013a118c565d8c
Andy Orahoske <andrew.orahoske@zipscene.com>
2015-05-02 13:22:19 -0400

    add a few more fields to recipes

--- app/models/recipe.js
+++ app/models/recipe.js
@@ -3,6 +3,10 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   description: DS.attr('string'),
+  prepTime: DS.attr('number'),
+  cookTime: DS.attr('number'),
+  servings: DS.attr('number'),
+  originalLink: DS.attr('string'),
   ingredients: DS.hasMany('ingredient', { async: true, inverse: 'recipe' }),
   steps: DS.hasMany('step', { async: true, inverse: 'recipe' })
 });
